03f067e2aef3626a1ba0482869e0a4f99fdd5255
winstonbarbe <winston.barbe@gmail.com>
2020-09-09 14:05:59 -0400

    adds path to all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,7 @@
 class Api::ProductsController < ApplicationController
+
+  def all_products_action
+    @records = Product.all
+    render "all_products.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products_path" => "products#all_products_action"
+  end
 end
